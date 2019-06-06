494cceff24a064c94afc0dbc5f659e4cbf5ec1ed
Vladislav <drimacus182@gmail.com>
2019-06-06 16:22:18 +0300

    array summ

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-Дуже важлива інфа
+//Дуже важлива інфа
+
+window.onload = function() {
+	console.log("i love git")
+}
+
+function summ(arr) {
+	return arr.reduce(function(o,v,i){ o = o + v return o;}, 0)
+}
+
